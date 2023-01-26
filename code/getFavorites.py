32fb52a2c56369ad2991c9331ccbcf113dda263a
Evan Woods <22337004+efwoods@users.noreply.github.com>
2023-01-26 17:15:35 -0500

    commit

--- code/getFavorites.py
+++ code/getFavorites.py
@@ -7,8 +7,10 @@ auth = tweepy.OAuthHandler(config['API_KEY'], config['API_KEY_SECRET'])
 auth.set_access_token(config['ACCESS_TOKEN'], config['ACCESS_TOKEN_SECRET'])
 api = tweepy.API(auth)
 
+username="EvanWoods"
+
 # Get list of the authenticated user's favorite tweets
-favorites = api.favorites()
+favorites = api.get_favorites(username,count=1)
 
 # Print the text of each tweet
 for favorite in favorites:
